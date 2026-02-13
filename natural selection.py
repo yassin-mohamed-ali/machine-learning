03e7b6f7a31731508b29270ec9928c35700c455f
Yassin mohamed ali <yassin2b2t@gmail.com>
2026-02-13 09:37:33 +0200

    .

--- natural selection.py
+++ natural selection.py
@@ -3,7 +3,7 @@ import numpy as np
 class Cat:
     def __init__(self, num_food,m_num_food,base,position):
         self.num_food = num_food
-        self.m_num_food = m_num_food
+        self.m_num_food = max(min(m_num_food, 5),1)
         self.base = base
         self.position = position
     def decisition(self):
@@ -18,8 +18,8 @@ class Cat:
                 self.position[1]-=1
             elif movement == "down":
                 self.position[1]+=1
-            self.position[0] = max(0, min(99, self.position[0]))
-            self.position[1] = max(0, min(99, self.position[1]))
+            self.position[0] = max(0, min(49, self.position[0]))
+            self.position[1] = max(0, min(49, self.position[1]))
         else:
             self.position = self.base.copy()
     def x(self):
@@ -35,7 +35,7 @@ class Cat:
         return False
     def reset(self):
         self.num_food = 0
-class fish:
+class Fish:
     def __init__(self, position):
         self.position = position
     def x(self):
@@ -46,7 +46,7 @@ def update():
     world = np.zeros((100, 100))
     cats_returned = 0
     cats_returned = 0
-    global fishes
+    global fishes, steps, generations
     for cat in cats[:]:
         world[cat.y(),cat.x()] = 1
         for fish in fishes[:]:
@@ -54,20 +54,24 @@ def update():
             if cat.position == fish.position:
                 cat.eat()
                 fishes.remove(fish)
-        cat.decisition()
+        if not cat.returned():
+            cat.decisition()
         if cat.returned():
             cats_returned+=1
-    if cats_returned > len(cats) * 0.75:
+            
+    if cats_returned > len(cats) * 0.75 or (steps >= 100) or len(fishes) == 0:
         for cat in cats[:]:
             if not cat.returned():
                 cats.remove(cat)
             else:
                 for i in range(cat.num_food):
-                    cats.append(Cat(0,cat.m_num_food,[random.randint(0,99),random.randint(0,99)],[random.randint(0,99),random.randint(0,99)]))
+                    cats.append(Cat(0,cat.m_num_food+random.randint(-1,1),[random.randint(0,49),random.randint(0,49)],[random.randint(0,49),random.randint(0,49)]))
                 cat.reset()
-        fishes = [fish([random.randint(0,99),random.randint(0,99)]) for _ in range(1,50)]
+        steps = 0
+        generations+=1
+        fishes = [Fish([random.randint(0,49),random.randint(0,49)]) for _ in range(1,min(generations*2,100))]
 def evaluate():
-    m1,m2,m3,m4,m5,m6,m7,m8,m9,m10 = [0,]*10
+    m1,m2,m3,m4,m5 = [0,]*5
     for cat in cats:
         x = cat.m_num_food
         if x == 1:
@@ -80,23 +84,18 @@ def evaluate():
             m4+=1
         elif x == 5:
             m5+=1
-        elif x == 6:
-            m6+=1
-        elif x == 7:
-            m7+=1
-        elif x == 8:
-            m8+=1
-        elif x == 9:
-            m9+=1
-        elif x == 10:
-            m10+=1
-    print(m1,m2,m3,m4,m5,m6,m7,m8,m9,m10)
+        
+    print(m1,m2,m3,m4,m5)
 
 
-cats = [Cat(0,random.randint(1,5),[random.randint(0,99),random.randint(0,99)],[random.randint(0,99),random.randint(0,99)]) for _ in range(1,50)]
-fishes = [fish([random.randint(0,99),random.randint(0,99)]) for _ in range(1,50)]
-world = np.zeros((100, 100))
-
+cats = [Cat(0,random.randint(1,5),[random.randint(0,49),random.randint(0,49)],[random.randint(0,49),random.randint(0,49)]) for _ in range(1,50)]
+fishes = [Fish([random.randint(0,49),random.randint(0,49)]) for _ in range(1,50)]
+world = np.zeros((50, 50))
+steps = 0
+generations = 1
 for i in range(1,1000):
     update()
-evaluate()
+    steps+=1
+    if i%10 == 0:
+        evaluate()
+print(f"num of generations: {generations}")
